b7a5abd7355ec73dba73aba81df6814acfbed374
Luan Ribeiro <luanribeiros@gmail.com>
2018-02-22 19:06:43 -0300

    crate new files for application and install and connection of the database

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import { express } from 'express';
+const express = require('express');
+const mysql = require('mysql2/promise');
 
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Olá FullStack Academy');
+  res.render('index');
 });
+
+const init = async () => {
+  const connection = await mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'futibaclub',
+  });
+  console.log(connection);
+};
+
+init();
 
 app.listen(3000, (err) => {
   if (err) {
