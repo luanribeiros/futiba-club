a9ffaf86500c14ea52fb6cba5a73618fba66e652
Luan Ribeiro <luanribeiros@gmail.com>
2018-02-22 16:51:46 -0300

    testing as session

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import express from 'express';
+import { express } from 'express';
+import { session } from 'express-session';
 
 const app = express();
+
+app.use(session({
+  secret: 'FullStack Academy',
+  saveUninitialized: true,
+  resave: true,
+}));
 
 app.get('/', (req, res) => {
   res.send('Olá FullStack Academy');
