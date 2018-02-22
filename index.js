3bce139ef34f071e0c3b1d49557fac03df20fd06
Luan Ribeiro <luanribeiros@gmail.com>
2018-02-22 15:41:45 -0300

    create server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app =  express();
+
+app.get('/', (req, res) => {
+  res.send('Olá FullStack Academy');
+});
+
+app.listen(3000, (err) => {
+  if(err) {
+    console.log('Não foi possível iniciar');
+  }
+  console.log('Servidor rodando ...');
+
+});
